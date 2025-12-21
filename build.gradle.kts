1ca441ef7796f480148bad74579c021c2877eba4
veildc <kaiyrzhan.de@gmail.com>
2025-12-21 11:37:18 +0500

    Add git-hooks, pre-push

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,22 @@ plugins {
     alias(libs.plugins.zenmoney.ktlint) apply true
     alias(libs.plugins.zenmoney.detekt) apply true
 }
+
+tasks.register<Copy>("installGitHooks") {
+    val gitHooksFileNames = listOf(
+        "pre-push",
+    )
+
+    group = "build setup"
+    description = "Installs Git hooks from scripts/githooks to .git/hooks/"
+    val hooksFilesToCopy = rootProject.files(gitHooksFileNames.map { fileName -> file("scripts/githooks/$fileName") })
+    from(hooksFilesToCopy)
+    val gitHooksDir = rootProject.file(".git/hooks")
+    into(gitHooksDir)
+
+    doLast {
+        rootProject.files(gitHooksFileNames.map { fileName -> File(gitHooksDir, fileName) })
+            .forEach { hookFile -> hookFile.setExecutable(true) }
+        println("✅ Git hook installed successfully!")
+    }
+}
